d9e936a269785f922f73d26b878a5c66e039de44
OrangeSoup <393664944@qq.com>
2021-04-25 12:59:31 +0800

    添加项目文件。

---
+++ DataAccess/IBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFBase.DataAccess
+{
+     interface IBase
+    {
+          object Add(object o);
+         object Delete(object o);
+         object Modify(object o);
+         object Get(object o);
+        object GetList(object o);
+    }
+}
---
+++ Model/ModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFBase.Model
+{
+   public class ModelBase
+    {
+        private bool _Status;
+        public bool Status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+
+
+        private DateTime _CreateDate;
+
+        public DateTime CreateDate
+        {
+            get { return _CreateDate; }
+            set { _CreateDate = value; }
+        }
+
+    }
+}
